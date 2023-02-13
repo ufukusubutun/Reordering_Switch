7114ba157a0037d4ed2ababcb355bda0ce9fc864
Ufuk Usubutun <ufukusubutun@gmail.com>
2023-02-13 15:06:10 -0500

    Commit before submitting for review

---
+++ rename.sh
@@ -0,0 +1,8 @@
+exp_save_name='exp1-alg1-RTT10-N8-lam5-numgen80-cap4500'
+#exp_save_name='exp1-alg3-RTT10-N8-lam5-cap4500'
+
+mv -i egress_cap1.pcap $exp_save_name-egress_cap1.pcap
+mv -i egress_cap2.pcap $exp_save_name-egress_cap2.pcap
+mv -i ingress_cap1.pcap $exp_save_name-ingress_cap1.pcap
+mv -i ingress_cap2.pcap $exp_save_name-ingress_cap2.pcap
+mv -i ingress_cap3.pcap $exp_save_name-ingress_cap3.pcap
