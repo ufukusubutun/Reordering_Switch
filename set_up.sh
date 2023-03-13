869f305d28c9d66d509b7ebc6ad167ab7eedbe49
Ufuk Usubutun <ufukusubutun@gmail.com>
2023-03-13 15:29:57 -0700

    setup update changes2

--- set_up.sh
+++ set_up.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# this is the modified new version after topology change decision
-# no bottleneck - 2 sinks
 
 update=$1
 
@@ -51,7 +49,7 @@ do
 	echo "sudo tc qdisc del dev $int2exp_sink root"
 	ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile "sudo tc qdisc del dev $int2exp_sink root"
 	ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile echo "${host} done!"
-	if [["$update" == "u"]];
+	if ["$update" == "u"];
 	then
 		ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile -f "sudo apt -y update; sudo apt-get -y update; sudo apt-get -y install iperf3 moreutils jq"
 	fi
@@ -72,7 +70,7 @@ do
 
 	ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile echo "${host} done!"
 
-	if [["$update" == "u"]];
+	if ["$update" == "u"];
 	then
 		ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile -f "sudo apt -y update; sudo apt-get -y update; sudo apt-get -y install iperf3 moreutils"
 	fi	
@@ -93,7 +91,7 @@ do
 		ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile "sudo tc qdisc del dev $int2node root"
 	done
 	ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile echo "${host} done!"
-	if [["$update" == "u"]];
+	if ["$update" == "u"];
 	then
 		ssh -oStrictHostKeyChecking=no ${uname}@${host} -i $keyfile -f "sudo apt -y update; sudo apt-get -y update; sudo apt-get -y install iperf3 moreutils"
 	fi
@@ -120,7 +118,7 @@ for i in $ifs; do
   sudo ethtool -K $i ufo off
 done
 
-if [["$update" == "u"]];
+if ["$update" == "u"];
 then
 	sudo apt -y update 
 	sudo apt-get -y update
@@ -146,7 +144,7 @@ int2node_gen 1 # any node would work
 echo "sudo tc qdisc del dev $int2node root"
 ssh -oStrictHostKeyChecking=no ${uname}@${exp_sink} -i $keyfile "sudo tc qdisc del dev $int2node root"
 ssh -oStrictHostKeyChecking=no ${uname}@$exp_sink -i $keyfile echo "${exp_sink} done!"
-if [["$update" == "u"]];
+if ["$update" == "u"];
 then
 	ssh -oStrictHostKeyChecking=no ${uname}@${exp_sink} -i $keyfile -f "sudo apt -y update; sudo apt-get -y update; sudo apt-get -y install iperf3 moreutils jq"
 fi
@@ -159,7 +157,7 @@ int2node_gen 1 # any node would work
 echo "sudo tc qdisc del dev $int2node root"
 ssh -oStrictHostKeyChecking=no ${uname}@${o_sink} -i $keyfile "sudo tc qdisc del dev $int2node root"
 ssh -oStrictHostKeyChecking=no ${uname}@$o_sink -i $keyfile echo "${o_sink} done!"
-if [["$update" == "u"]];
+if ["$update" == "u"];
 then
 	ssh -oStrictHostKeyChecking=no ${uname}@${o_sink} -i $keyfile -f "sudo apt -y update; sudo apt-get -y update; sudo apt-get -y install iperf3 moreutils jq"
 fi
